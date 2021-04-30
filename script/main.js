a7a6d4231af75e513a3e5ed181e8af05d757b5b9
da4a <nomokon712@gmail.com>
2021-04-30 15:18:31 +0700

    add img, scripts

--- script/main.js
+++ script/main.js
@@ -6,41 +6,40 @@ window.onscroll = function() {
 }
 
 
-
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(45, window.innerHeight / window.innerHeight, 0.1, 1000);
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(45, window.innerHeight / window.innerHeight, 0.1, 1000);
 camera.position.z = 10;
 
-renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+let renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setClearColor(0x000000, 0);
 renderer.setSize(1280, 720);
 
 renderer.domElement.setAttribute("id", "gamePC");
 document.body.insertBefore(renderer.domElement, document.body.firstChild);
 
-const aLight = new THREE.AmbientLight(0x404040, 1.2);
+const aLight = new THREE.AmbientLight(0x404040, 2.6);
 scene.add(aLight);
 
-const pLight = new THREE.PointLight(0xFFFFFF, 1.2);
-pLight.position.set(0, -3, 7);
+const pLight = new THREE.PointLight(0xFFFFFF, 1.6);
+pLight.position.set(0, -4, 7);
 scene.add(pLight);
 
-const helper = new THREE.PointLightHelper(pLight);
-scene.add(helper);
+//const helper = new THREE.PointLightHelper(pLight);
+//scene.add(helper);
 
 let loader = new THREE.GLTFLoader();
 let obj = null;
 
-loader.load('./image/model/pc2/scene.gltf', function(gltf) {
+loader.load('./image/model/pc1/scene.gltf', function(gltf) {
     obj = gltf;
-    obj.scene.scale.set(0.5, 0.5, 0.4);
+    obj.scene.scale.set(0.5, 0.6, 0.5);
     scene.add(obj.scene);
 });
 
 function animate() {
     requestAnimationFrame(animate);
     if (obj)
-        obj.scene.rotation.y += 0.01;
+        obj.scene.rotation.y += 0.003;
     renderer.render(scene, camera);
 }
 animate();
