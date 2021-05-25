f97f016b8fc1acfe292d2847c8b2fa651717d444
da4a <nomokon712@gmail.com>
2021-05-25 22:45:01 +0700

    add get

---
+++ script/basket.js
@@ -0,0 +1,222 @@
+const cart = {
+    "001": {
+        "name": "Standart edition",
+        "price": 55000,
+    },
+    "002": {
+        "name": "Lumen",
+        "price": 78000,
+    },
+    "003": {
+        "name": "Lumen+",
+        "price": 82000,
+    },
+    "004": {
+        "name": "150 fps +",
+        "price": 96300,
+    },
+    "005": {
+        "name": "NZXT Edition",
+        "price": 110000,
+    },
+    "006": {
+        "name": "Water plus",
+        "price": 123400,
+    },
+    "007": {
+        "name": "Progressive",
+        "price": 145600,
+    },
+    "008": {
+        "name": "Standart",
+        "price": 65800,
+    },
+    "009": {
+        "name": "Lumen X",
+        "price": 95000,
+    },
+    "010": {
+        "name": "Lumen XL",
+        "price": 149900,
+    },
+    "011": {
+        "name": "Mega super laptop",
+        "price": 156800,
+    },
+    "012": {
+        "name": "S version",
+        "price": 78000,
+    },
+    "013": {
+        "name": "X version",
+        "price": 92000,
+    },
+    "014": {
+        "name": "L version",
+        "price": 120400,
+    },
+    "015": {
+        "name": "M version",
+        "price": 154300,
+    },
+    "016": {
+        "name": "PS4 Pro",
+        "price": 34000,
+    },
+    "017": {
+        "name": "PS5",
+        "price": 49600,
+    },
+    "018": {
+        "name": "Forest Edition",
+        "price": 35500,
+    },
+    "019": {
+        "name": "CS:GO Edition",
+        "price": 35500,
+    },
+    "020": {
+        "name": "Toxin",
+        "price": 35500,
+    },
+    "021": {
+        "name": "ASUS ROG Pugio II",
+        "price": 6500,
+    },
+    "022": {
+        "name": "Sky Dragon",
+        "price": 8500,
+    },
+    "023": {
+        "name": "Defender Shock",
+        "price": 9500,
+    },
+    "024": {
+        "name": "Defender Titan",
+        "price": 10500,
+    },
+    "025": {
+        "name": "Razer DeathAdder",
+        "price": 11500,
+    },
+    "026": {
+        "name": "Mouse Grip Tapes",
+        "price": 12500,
+    },
+    "027": {
+        "name": "Razer Basilisk",
+        "price": 13500,
+    },
+    "028": {
+        "name": "RAZER BLACKWIDOW",
+        "price": 5600,
+    },
+    "029": {
+        "name": "Razer Huntsman Mini",
+        "price": 7200,
+    },
+    "030": {
+        "name": "ghost k1 belle delphine edition",
+        "price": 8100,
+    },
+    "031": {
+        "name": "ghost k1 akki inferno edition",
+        "price": 9500,
+    },
+    "032": {
+        "name": "Razer BlackWidow Ultimate",
+        "price": 12500,
+    },
+    "033": {
+        "name": "Logitech LIGHTSPEED",
+        "price": 5500,
+    },
+    "034": {
+        "name": "Beats EP",
+        "price": 6500,
+    },
+    "035": {
+        "name": "Razer Arigato",
+        "price": 6700,
+    },
+    "036": {
+        "name": "Razer Black Monster",
+        "price": 7500,
+    },
+    "037": {
+        "name": "Razer beats 2x",
+        "price": 8200,
+    },
+    "038": {
+        "name": "Asus strix 2.0",
+        "price": 9500,
+    },
+    "039": {
+        "name": "Xiaomi gaming X",
+        "price": 12000,
+    },
+    "040": {
+        "name": "HyperX QuadCast S",
+        "price": 4500,
+    },
+    "041": {
+        "name": "HyperX QuadCast X",
+        "price": 6500,
+    },
+    "042": {
+        "name": "HyperX QuadCast L",
+        "price": 8500,
+    },
+    "043": {
+        "name": "HyperX QuadCast",
+        "price": 9000,
+    },
+    "044": {
+        "name": "HyperX QuadCast K",
+        "price": 9900,
+    },
+    "045": {
+        "name": "HyperX QuadCast +",
+        "price": 12000,
+    },
+    "046": {
+        "name": "HyperX QuadCast XXL",
+        "price": 14500,
+    },
+    "047": {
+        "name": "PS4 DualShock 4",
+        "price": 4500,
+    },
+    "048": {
+        "name": "PS5 DualSense Wireless",
+        "price": 4500,
+    },
+    "049": {
+        "name": "Acer asper 21",
+        "price": 12900,
+    },
+    "050": {
+        "name": "Aser g450S",
+        "price": 15600,
+    },
+    "051": {
+        "name": "AOC Game+",
+        "price": 17900,
+    },
+    "052": {
+        "name": "MSI Gaming g400",
+        "price": 20500,
+    },
+    "053": {
+        "name": "MSI RGB GSPORT",
+        "price": 25600,
+    },
+    "054": {
+        "name": "MSI FULLSTRICK",
+        "price": 35000,
+    },
+    "055": {
+        "name": "ASUS ROG F450",
+        "price": 45600,
+    },
+};
